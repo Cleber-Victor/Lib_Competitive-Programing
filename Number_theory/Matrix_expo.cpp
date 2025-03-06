12994e9ee4b455b0fe0aa01e3bf09d9e471b77a8
Cleber Victor <74624346+Cleber-Victor@users.noreply.github.com>
2025-03-06 20:05:50 -0300

    Update Matrix_expo.cpp

--- Number_theory/Matrix_expo.cpp
+++ Number_theory/Matrix_expo.cpp
@@ -1,59 +1,43 @@
-#include<bits/stdc++.h>
- 
-using namespace std;
-#define pii pair<int, int>
-#define pll pair<long long, long long>
-#define vi vector<int>
-#define vll vector<long long>
- 
-typedef long long ll;
- 
-const ll MOD = 1e9 +7;;
-ll m,n,k;
-vector<vll>bse;
- 
-struct Matrix{
-  vector<vll> a;
-  Matrix operator * (const Matrix  &p){
-    Matrix product;
-		product.a = bse;
-    for (int i = 0; i < n; i++){
-      for (int j = 0; j< n; j++){
-        for (int k = 0; k < n; k++){
-          product.a[i][j] = (product.a[i][j]+ ((a[i][k] * p.a[k][j])%MOD))%MOD ;
-        }
-      }
-    }  
-    return product;
-  }   
+// Fast Exp
+const ll mod = 1e9+7;
+
+
+ll fexpll(ll a, ll n){
+	ll ans = 1;
+	while(n){
+		if(n & 1) ans = (ans * a) % mod;
+		a = (a * a) % mod;
+		n >>= 1;
+	}
+	return ans;
+}
+// matriz quadrada
+class Matrix{ 
+	public:
+	vector<vector<ll>> mat;
+	int m;
+	Matrix(int m): m(m){
+		mat.resize(m);
+		for(int i = 0; i < m; i++) mat[i].resize(m,0);
+	}
+	Matrix operator * (const Matrix& rhs){
+		Matrix ans = Matrix(m);
+		for(int i = 0; i < m; i++)
+			for(int j = 0; j < m; j++)
+				for(int k = 0; k < m; k++)
+					ans.mat[i][j] = (ans.mat[i][j] + (mat[i][k] * rhs.mat[k][j]) % mod) % mod;
+		return ans;
+	}
 };
 
-int main(){
-  ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin>>n>>k;  // n é o lado da matriz quadrada
-	ll ans;
-	Matrix base;   // matriz de recorrencia
-	Matrix resp;   // matriz unitaria
-	bse.assign(n,vll(n,0));  // necessario para a Matrix product;
-	resp.a.resize(n);
-	vi a(n);
-	for(int i =0; i <n;i++){
-		cin >>a[i];
+Matrix fexp(Matrix a, ll n){
+	int m = a.m;
+	Matrix ans = Matrix(m);
+	for(int i = 0; i < m; i++) ans.mat[i][i] = 1;
+	while(n){
+		if(n & 1) ans = ans * a;
+		a = a * a;
+		n >>= 1;
 	}
-	for(int i =0; i <n;i++){
-		resp.a[i].resize(n);
-		for(int j =0; j <n;j++){
-			if(i==j){
-				resp.a[i][j]=1;
-			}else{
-				resp.a[i][j]=0;
-			}
-		}
-	}
-	base.a.resize(n);
-
-
-	return 0;
+	return ans;
 }
- 
