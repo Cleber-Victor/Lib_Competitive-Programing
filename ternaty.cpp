0ed5c806fd69bcabc2256f1a5f0a27270013fbe3
Cleber Victor <74624346+Cleber-Victor@users.noreply.github.com>
2025-04-05 10:12:26 -0300

    Update ternaty.cpp

--- ternaty.cpp
+++ ternaty.cpp
@@ -1,38 +1,50 @@
 #include<bits/stdc++.h>
- 
+
 using namespace std;
 #define pii pair<int, int>
 #define pll pair<long long, long long>
 #define vi vector<int>
 #define vll vector<long long>
- 
-typedef long long ll;
 
-const ll MOD = 1e9 +7;
+const long long MOD = 1000000007;
+
+const double eps = 1e-6;
+
+double b,c;
+double f(double m){
+	double ret = (m*m + b*m + c)/sin(m);
+	
+	return ret;
+}
 
 
 
 int main(){
   ios::sync_with_stdio(0);
-	cin.tie(0);
-  string s; cin >>s;
-  vi prf(s.size()+1,0);
-  for(int i =0; i <s.size();i++){
-    if(i==s.size()-1){
-      prf[i+1] = prf[i];
-    }else if (s[i+1] ==s[i]){
-      prf[i+1]= prf[i]+1;
-    }else{
-      prf[i+1]=prf[i];
-    }
-  }
-  int q; cin >>q;
-  for(int i =0; i <q;i++){
-    int l,r; cin >>l>>r;
-    l--;
-    r--;
-    int ans = prf[r]-prf[l];
-    cout <<ans<<'\n';
-  }
-	return 0;
+  cin.tie(NULL);
+	int t; cin >>t;
+
+	while(t--){
+		//cin >>b>>c;	
+		double l,r;
+		cin>>b>>c;
+		l = 0.000001;
+		r = (M_PI/2);
+		double divide = 3;
+		while(r-l > eps){
+			double m1 = l + ((r-l)/divide);
+			double m2 = r - ((r-l)/divide);
+			if(f(m2)<=f(m1)){
+				l = m1;				
+			}else{
+				r = m2;
+			}
+		}
+		double div =2;
+		double ans = f((l+r)/div);	
+		cout << setprecision(10)<<fixed;
+		cout <<ans<<'\n';
+	}
+    
+  return 0;
 }
